3404dfe0d4ebaf5e0db83581569feea37157b16a
wjdwltn <jlsowltn2000@naver.com>
2025-03-12 22:15:26 +0900

    초기세팅

---
+++ src/main/java/com/wltn/foodflow/FoodFlowApplication.java
@@ -0,0 +1,13 @@
+package com.wltn.foodflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wltn/foodflow/FoodFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wltn.foodflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
